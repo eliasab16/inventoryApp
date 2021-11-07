f78d9133975486408b78c92832cef919bae4ece2
eliasab16 <eliasab@college.harvard.edu>
2021-11-06 20:50:42 -0400

    Initial Commit

---
+++ inventoryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  inventoryApp
+//
+//  Created by Elias Abunuwara on 2021-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ inventoryApp/inventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  inventoryAppApp.swift
+//  inventoryApp
+//
+//  Created by Elias Abunuwara on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct inventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
